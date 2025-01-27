42e83c4ae44affd601a27c5c11d502a6d42d3bca
Guilherme Felipe de Lima <guifellim20@gmail.com>
2025-01-27 16:52:37 +0100

    arrumando titulos

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import { Heart } from 'lucide-react';
+import { Gift, Heart } from 'lucide-react';
 
 export function Header() {
   return (
     <header className="bg-white shadow-md">
       <div className="max-w-7xl mx-auto px-4 py-6 sm:px-6 lg:px-8">
-        <div className="flex items-center space-x-3">
+        <div className="flex items-center justify-center space-x-3">
           <Heart className="w-8 h-8 text-sky-500" />
-          <h1 className="text-3xl font-semibold text-gray-900">
-            Larissa & Guilherme
+          <Gift className="w-8 h-8 text-sky-500" />
+          <h1 className="text-3xl font-semibold text-center text-gray-900">
+           Lista de Presentes Larissa & Guilherme
           </h1>
+          <Gift className="w-8 h-8 text-sky-500" />
+          <Heart className="w-8 h-8 text-sky-500" />
         </div>
       </div>
     </header>
